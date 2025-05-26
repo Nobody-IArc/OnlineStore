320a17007d0686e4a8605a2995e2962b98877446
Nobody-IArc <utk3970@naver.com>
2025-05-26 17:06:11 +0900

    계정 기능 구현 완료

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<script setup>
+import {reactive} from "vue";
+import {login} from "@/services/accountService";
+import {useRouter} from "vue-router";
+
+// 반응형 상태
+const state = reactive({
+  form: {
+    loginId: "",
+    loginPw: "",
+  }
+});
+
+// 라우터 객체
+const router = useRouter();
+
+// 로그인 데이터 전송
+const submit = async() => {
+  const res = await login(state.form);
+
+  switch(res.status) {
+    case 200:
+      await router.push("/");
+      break;
+    case 404:
+      window.alert("Member Not Found");
+      break;
+  }
+};
+
+</script>
+
+<template>
+  <div class="login">
+    <div class="container">
+      <form class="py-5 d-flex flex-column gap-3" @submit.prevent="submit">
+        <h1 class="h5 mb-3">Login</h1>
+        <div class="form-floating">
+          <input type="email" class="form-control" id="loginId"
+                 placeholder="E-mail" v-model="state.form.loginId">
+          <label for="loginId">E-mail</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="loginPw"
+                 placeholder="Password" v-model="state.form.loginPw">
+          <label for="loginPw">Password</label>
+        </div>
+        <button type="submit" class="w-100 h6 btn py-3 btn-primary">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login > .container {
+  max-width: 576px;
+}
+</style>
