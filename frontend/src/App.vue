9d5961aa3aa255c2f82e71775b34a58f7ce08b40
Nobody-IArc <utk3970@naver.com>
2025-05-26 15:39:57 +0900

    프런트엔드 레이아웃 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,33 @@
 <script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import {useAccountStore} from "@/stores/account";
+import {watch} from "vue";
+import {useRoute} from "vue-router";
+
+const accountStore = useAccountStore();
+
+const route = useRoute();
+
+const checkAccount = async() => {
+  // 추후
+}
+
+// 커스텀 생성 훅
+(async function onCreated() {
+  await checkAccount();
+})();
+
+// 라우트 경로 변경 시 로그인 여부 확인하는 로직
+watch(() => route.path, () => {
+  checkAccount();
+});
 </script>
 
 <template>
+  <Header />
+  <main>
+    <router-view></router-view>
+  </main>
+  <Footer />
 </template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup>
+// 로그아웃
+const logoutAccount = async () => {
+  window.alert("Non-Ready");
+};
+</script>
+
+<template>
+  <header>
+    <div class="navbar navbar-dark bg-dark text-white shadow-sm">
+      <div class="container">
+        <router-link to ="/" class="navbar-brand">
+          <strong>Online Store</strong>
+        </router-link>
+        <div class="menus d-flex gap-3">
+          <template v-if="true">
+            <router-link to="/login">Login</router-link>
+            <router-link to="/join">Join</router-link>
+          </template>
+          <template v-else>
+            <a @click="logoutAccount()">Logout</a>
+            <router-link to="/orders">Orders</router-link>
+            <router-link to="/cart">Cart</router-link>
+          </template>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  .menus {
+    a {
+      cursor: pointer;
+      color: #fff;
+      text-decoration: none;
+    }
+  }
+}
+</style>
