618c5b566c00b9cc06b4e9a94947629a5a738017
NoBody <utk3970@naver.com>
2025-05-28 22:03:41 +0900

    Update AccountConstants.java

--- src/main/java/com/damon/onlinestore/account/etc/AccountConstants.java
+++ src/main/java/com/damon/onlinestore/account/etc/AccountConstants.java
@@ -2,19 +2,21 @@ package com.damon.onlinestore.account.etc;
 
 public class AccountConstants {
 
+
+    // Github 업로드는 상수 가림
     // Member ID
-    public static final String MEMBER_ID_NAME = "memberAccount";
+    public static final String MEMBER_ID_NAME;
 
     // Access Token Name
-    public static final String ACCESS_TOKEN_NAME = "accessToken";
+    public static final String ACCESS_TOKEN_NAME;
 
     // Refresh Token Name
-    public static final String REFRESH_TOKEN_NAME = "refreshToken";
+    public static final String REFRESH_TOKEN_NAME;
 
     // Access Token Life Time
-    public static final int ACCESS_TOKEN_EXP_MINUTES = 1;
+    public static final int ACCESS_TOKEN_EXP_MINUTES;
 
     // Refresh Token Life Time
-    public static final int REFRESH_TOKEN_EXP_MINUTES = 60 * 24;
+    public static final int REFRESH_TOKEN_EXP_MINUTES;
 
 }
